bc6cd3ea6c4a68b23ff28697177f95572efa31b2
Lsong <song940@gmail.com>
2018-03-22 14:29:24 +0800

    modify

--- index.js
+++ index.js
@@ -16,96 +16,30 @@ function DNS(options){
       '114.114.114.114'
     ],
     rootServers: [
-      'a.root-servers.net',
-      'b.root-servers.net',
-      'c.root-servers.net',
-      'd.root-servers.net',
-      'e.root-servers.net',
-      'f.root-servers.net',
-      'g.root-servers.net',
-      'h.root-servers.net',
-      'i.root-servers.net',
-      'j.root-servers.net',
-      'k.root-servers.net',
-      'l.root-servers.net',
-      'm.root-servers.net'
-    ]
+      'a','b','c','d','e','f',
+      'g','h','i','j','k','l','m'
+    ].map(x => `${x}.root-servers.net`)
   }, options);
-  var self = this;
-  this.requests = [];
-  this.servers = this.options.servers.map(function(ns){
-    return { server: ns, port: this.options.port, priority: 10 };
-  }.bind(this));
-  this.socket = udp.createSocket('udp4');
-  this.socket.on('message', function(message, rinfo){
-    var response = Packet.parse(message);
-    self.requests.forEach(function(request){
-      if(response.header.id === request.header.id){
-        clearInterval(request.timer);
-        request.done(null, response);
-        self.socket.close();
-      }else{
-        console.warn('response id mismatch %s, %s', 
-          request.header.id, response.header.id);
-      }
-    });
-  });
   return this;
 }
 
-DNS.prototype.query = (domain, nameservers, done) => {
-  console.log(domain, nameservers);
+DNS.prototype.query = function(domain){
+  return this.resolve(domain);
 };
 
-DNS.prototype.resolve = function(domain){
-  const parts = domain.split('.');
-  (function resolve(ns){
-    this.query(parts.pop(), ns, resolve);
-  }.bind(this))(this.rootServers);
+DNS.prototype.send = function(packet){
+  console.log(this.rootServers);
 };
 
-/**
- * [send description]
- * @param  {[type]} request [description]
- * @return {[type]}         [description]
- */
-DNS.prototype.send = function(request, callback){
-  var self = this;
-  request.times = 0;
-  request.done = callback;
-  request.socket = this.socket;
-  request.send = function(){
-    this.requestTime = new Date;
-    this.header.rd = 1;
-    this.header.id = DNS.Packet.uuid();
-    this.server = self.servers.sort(function(a, b){
-      return a.priority - b.priority;
-    })[0];
-    var buffer = this.toBuffer();
-    this.socket.send(buffer, 0, buffer.length,
-    this.server.port,
-    this.server.server, function(err, len){
-      console.log('packet sent %s bytes to %s', len, request.server.server);
-    });
-    return this;
-  };
-  request.timer = setInterval(function(){
-    if(request.times < self.options.retries){
-      self.servers = self.servers.map(function(a){
-        if(request.server.server === a.server){
-          a.priority++;
-        }
-        return a;
-      });
-      request.send();
-      request.times++;
-    }else{
-      clearInterval(request.timer);
-      request.done(new Error('timeout'));
-    }
-  }, this.options.timeout * 1e3);
-  this.requests.push(request.send());
-  return this;
+DNS.prototype.resolve = function(domain, type, ns){
+  // ns = ns || this.rootServers;
+  // const records = await resolve(domain);
+  // if(A in records){
+  //   return records;
+  // }else{
+  //   const { ns } = records;
+  //   return resolve(domain, type, ns);
+  // }
 };
 
 /**
