612972d3d4572d13ee437dc3f1ad159401936b99
Martin Heidegger <martin.heidegger@gmail.com>
2021-06-14 22:45:03 +0900

    test: improving test framework to execute tests serially and better identify operational order

--- test/test.js
+++ test/test.js
@@ -1,27 +1,34 @@
 const { inspect } = require('util');
+
+let previous = Promise.resolve();
+
 /**
  * super tiny testing framework
  *
  * @author Liu song <hi@lsong.org>
  * @github https://github.com/song940
  */
-const test = async(title, fn) => {
-  try {
-    await fn();
-    console.log(color(` ✔  ${title}`, 32));
-  } catch (err) {
-    console.error(color(` ✘  ${title}`, 31));
-    console.log();
-    console.log(color(`   ${err.name}: ${err.message}`, 31));
-    console.error(color(`   expected: ${inspect(err.expected)}`, 32));
-    console.error(color(`     actual: ${inspect(err.actual)}`, 31));
-    console.log();
-    process.exit(1);
-  }
+const test = (title, fn) => {
+  previous = previous.then(async() => {
+    try {
+      await fn();
+      console.log(color(` ✔  ${title}`, 32));
+    } catch (err) {
+      console.error(color(` ✘  ${title}`, 31));
+      console.log();
+      console.log(color(`   ${err.name}: ${err.message}`, 31));
+      console.error(color(`   expected: ${inspect(err.expected)}`, 32));
+      console.error(color(`     actual: ${inspect(err.actual)}`, 31));
+      console.log(err.stack);
+      console.log();
+      process.exit(1);
+    }
+  });
+  return previous;
 };
 
 function color(str, c) {
-  return '\x1b[' + c + 'm' + str + '\x1b[0m';
+  return `\x1b[${c}m${str}\x1b[0m`;
 }
 
 module.exports = test;
