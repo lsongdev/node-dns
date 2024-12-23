5aa630fecf21c861e10bd4992d7e2d861b16b6f6
Lsong <song940@gmail.com>
2024-12-23 22:43:46 +0800

    add dns over tls supports, and fix #88

--- client/tcp.js
+++ client/tcp.js
@@ -1,34 +1,48 @@
+const tls = require('tls');
 const tcp = require('net');
 const Packet = require('../packet');
 
-module.exports = ({ dns = '1.1.1.1', port = 53 } = {}) => {
-  return async(name, type = 'A', cls = Packet.CLASS.IN, { clientIp, recursive = true } = {}) => {
-    const packet = new Packet();
+const makeQuery = ({ name, type = 'A', cls = Packet.CLASS.IN, clientIp, recursive = true }) => {
+  const packet = new Packet();
+  packet.header.rd = recursive ? 1 : 0;
+  
+  if (clientIp) {
+    packet.additionals.push(Packet.Resource.EDNS([
+      Packet.Resource.EDNS.ECS(clientIp)
+    ]));
+  }
+  
+  packet.questions.push({ name, class: cls, type: Packet.TYPE[type] });
+  return packet.toBuffer();
+};
 
-    // see https://github.com/song940/node-dns/issues/29
-    if (recursive) {
-      packet.header.rd = 1;
-    }
-    if (clientIp) {
-      packet.additionals.push(Packet.Resource.EDNS([
-        Packet.Resource.EDNS.ECS(clientIp),
-      ]));
-    }
+const sendQuery = (client, message) => {
+  const len = Buffer.alloc(2);
+  len.writeUInt16BE(message.length);
+  client.end(Buffer.concat([len, message]));
+};
 
-    packet.questions.push({
-      name,
-      class : cls,
-      type  : Packet.TYPE[type],
-    });
-    const message = packet.toBuffer();
-    const len = Buffer.alloc(2);
-    len.writeUInt16BE(message.length);
-    const client = tcp.connect({ host: dns, port });
-    client.end(Buffer.concat([ len, message ]));
+const protocols = {
+  'tcp:': (host, port) => tcp.connect({ host, port }),
+  'tls:': (host, port) => tls.connect({ host, port, servername: host })
+};
+
+const TCPClient = ({ dns, protocol = 'tcp:', port = protocol === 'tls:' ? 853 : 53 } = {}) => {
+  if (!protocols[protocol]) {
+    throw new Error('Protocol must be tcp: or tls:');
+  }
+
+  return async (name, type, cls, options = {}) => {
+    const message = makeQuery({ name, type, cls, ...options });
+    const [host] = dns.split(':');
+    const client = protocols[protocol](host, port);
+    
+    sendQuery(client, message);
     const data = await Packet.readStream(client);
-    if (!data.length) {
-      throw new Error('Empty TCP response');
-    }
+    
+    if (!data.length) throw new Error('Empty response');
     return Packet.parse(data);
   };
 };
+
+module.exports = TCPClient;
