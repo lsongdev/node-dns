a9d3400b80b59ff068cc4b246bc584ceb37fe7ec
Lsong <song940@gmail.com>
2024-12-23 22:39:05 +0800

    add h2(http2) supports, and fix #89

--- client/doh.js
+++ client/doh.js
@@ -1,49 +1,84 @@
+const http = require('http');
+const https = require('https');
+const http2 = require('http2');
 const Packet = require('../packet');
 
-const defaultGet = url => new Promise((resolve, reject) => {
-  const headers = {
-    accept: 'application/dns-message',
-  };
-  const base = url.startsWith('https') ? require('https') : require('http');
-  const req = base.get(url, { headers }, resolve);
-  req.on('error', reject);
-});
+const protocols = {
+  'http:': http.get,
+  'https:': https.get,
+  'h2:': (url, options, cb) => {
+    const urlObj = new URL(url);
+    const client = http2.connect(url.replace('h2:', 'https:'));
+    const req = client.request({
+      ':path': `${urlObj.pathname}${urlObj.search}`,
+      ':method': 'GET',
+      ...options.headers
+    });
 
-const readStream = stream => {
-  const buffer = [];
-  return new Promise((resolve, reject) => {
-    stream
-      .on('error', reject)
-      .on('data', chunk => buffer.push(chunk))
-      .on('end', () => resolve(Buffer.concat(buffer)));
-  });
+    req.on('response', headers => {
+      client.close();
+      cb({
+        headers,
+        statusCode: headers[':status'],
+        on: req.on.bind(req)
+      });
+    });
+
+    req.on('error', err => {
+      client.close();
+      throw err;
+    });
+
+    req.end();
+  }
 };
 
-/**
- * @docs https://tools.ietf.org/html/rfc8484
- * @param {*} param0
- */
-const DOHClient = ({ dns, http, get = defaultGet } = {}) => {
-  return (name, type = 'A', cls = Packet.CLASS.IN, { clientIp, recursive = true } = {}) => {
-    const packet = new Packet();
-    // see https://github.com/song940/node-dns/issues/29
-    if (recursive) {
-      packet.header.rd = 1;
-    }
-    if (clientIp) {
-      packet.additionals.push(Packet.Resource.EDNS([
-        Packet.Resource.EDNS.ECS(clientIp),
-      ]));
-    }
-    packet.questions.push({
-      name,
-      class : cls,
-      type  : Packet.TYPE[type],
+const makeRequest = (url, query) => new Promise((resolve, reject) => {
+  const index = url.indexOf('://');
+  if (index === -1) url = `https://${url}`;
+  const u = new URL(url);
+  const get = protocols[u.protocol];
+  if (!get) throw new Error(`Unsupported protocol: ${u.protocol}, must be specified (http://, https:// or h2://)`);
+  if (!u.pathname) url += '/dns-query?dns={query}';
+  url = url.replace('{query}', query);
+  const req = get(url, { headers: { accept: 'application/dns-message' } }, resolve);
+  if (req) req.on('error', reject);
+});
+
+const readStream = res => new Promise((resolve, reject) => {
+  const chunks = [];
+  res
+    .on('error', reject)
+    .on('data', chunk => chunks.push(chunk))
+    .on('end', () => {
+      const data = Buffer.concat(chunks);
+      if (res.statusCode !== 200) {
+        reject(new Error(`HTTP ${res.statusCode}: ${data.toString()}`));
+      }
+      resolve(data);
     });
-    const query = packet.toBase64URL();
-    return Promise.resolve(get(`http${http ? '' : 's'}://${dns}/dns-query?dns=${query}`))
-      .then(readStream)
-      .then(Packet.parse);
+});
+
+const buildQuery = ({ name, type = 'A', cls = Packet.CLASS.IN, clientIp, recursive = true }) => {
+  const packet = new Packet();
+  packet.header.rd = recursive ? 1 : 0;
+
+  if (clientIp) {
+    packet.additionals.push(Packet.Resource.EDNS([
+      Packet.Resource.EDNS.ECS(clientIp)
+    ]));
+  }
+
+  packet.questions.push({ name, class: cls, type: Packet.TYPE[type] });
+  return packet.toBase64URL();
+};
+
+const DOHClient = ({ dns }) => {
+  return async (name, type, cls, options = {}) => {
+    const query = buildQuery({ name, type, cls, ...options });
+    const response = await makeRequest(dns, query);
+    const data = await readStream(response);
+    return Packet.parse(data);
   };
 };
 
--- example/client/doh.js
+++ example/client/doh.js
@@ -1,12 +1,22 @@
 const { DOHClient } = require('../..');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const resolve = DOHClient({
-  dns: '1.1.1.1',
-});
+// const resolve = DOHClient({
+//   dns: '1.1.1.1',
+// });
 
-(async() => {
-  const response = await resolve('google.com');
-  console.log(response.answers);
-})();
+// (async() => {
+//   const response = await resolve('google.com');
+//   console.log(response.answers);
+// })();
+
+// import DNS2 from 'dns2';
+
+// DOHClient({
+//   dns: 'h2://ada.openbld.net',
+// })('cdnjs.com', 'NS').then(console.log);
+
+DOHClient({
+  dns: 'h2://ada.openbld.net/dns-query?dns={query}',
+})('cdnjs.com', 'NS').then(console.log);
